20a154167c8233bad95d1f86ebeeccc99698a2a2
illarionovEA <egor.mypost@gmail.com>
2026-04-14 15:58:05 +0300

    upd docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.join("../.."))
+sys.path.insert(0, os.path.join(".."))
 import geocode
 
 # -- Project information -----------------------------------------------------
